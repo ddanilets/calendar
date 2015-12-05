e2cc53766f339c68a360ecc75270dd344d7dd756
ddanilets <ddanilets@gmail.com>
2015-12-05 02:45:33 +0200

    Signed-off-by: ddanilets <ddanilets@gmail.com>

--- react/script/dispatcher/FBDispatcher.js
+++ react/script/dispatcher/FBDispatcher.js
@@ -1,5 +1,22 @@
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
+var _createClass = (function () {
+	function defineProperties(target, props) {
+		for (var i = 0; i < props.length; i++) {
+			var descriptor = props[i];
+			descriptor.enumerable = descriptor.enumerable || false;
+			descriptor.configurable = true; 
+			if ('value' in descriptor) 
+				descriptor.writable = true; 
+			Object.defineProperty(target, descriptor.key, descriptor); 
+			} 
+		} 
+	return function (Constructor, protoProps, staticProps) {
+		if (protoProps) 
+			defineProperties(Constructor.prototype, protoProps); 
+		if (staticProps) defineProperties(Constructor, staticProps); 
+			return Constructor; 
+	}; 
+	});
+_createClass();
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var Dispatcher = (function () {
@@ -129,4 +146,4 @@ var Dispatcher = (function () {
   }]);
 
   return Dispatcher;
-});
+})();
