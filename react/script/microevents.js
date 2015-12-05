541372e692a26b0188e173dfc46f8f8074934757
ddanilets <ddanilets@gmail.com>
2015-12-05 06:38:30 +0200

    Signed-off-by: ddanilets <ddanilets@gmail.com>

--- react/script/microevents.js
+++ react/script/microevents.js
@@ -9,18 +9,19 @@
  *   - make it safer to use
 */
 
-var MicroEvent	= function(){
-	this.bind = function(event, fct){
+var MicroEvent	= function(){};
+MicroEvent.prototype	= {
+	bind	: function(event, fct){
 		this._events = this._events || {};
 		this._events[event] = this._events[event]	|| [];
 		this._events[event].push(fct);
 	},
-	this.unbind = function(event, fct){
+	unbind	: function(event, fct){
 		this._events = this._events || {};
 		if( event in this._events === false  )	return;
 		this._events[event].splice(this._events[event].indexOf(fct), 1);
 	},
-	this.trigger	= function(event /* , args... */){
+	trigger	: function(event /* , args... */){
 		this._events = this._events || {};
 		if( event in this._events === false  )	return;
 		for(var i = 0; i < this._events[event].length; i++){
