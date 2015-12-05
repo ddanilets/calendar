d1b8028bb329cd844bd3676e6b7223dbe4ca9ebd
ddanilets <ddanilets@gmail.com>
2015-12-05 08:10:26 +0200

    Signed-off-by: ddanilets <ddanilets@gmail.com>

--- react/script/components/month.js
+++ react/script/components/month.js
@@ -46,8 +46,6 @@ var Month = React.createClass({
 				monthName: days1[index].monthName,
 				num: days1[index].num,
 			}
-			alert (days1[index].num);
-			alert (value.num);
 		});
 
 		var tasks = days.map(function(day, index) {
