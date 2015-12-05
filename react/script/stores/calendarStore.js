4e42abf5ebd2c2229d127ea0f9be8d80bae286ff
ddanilets <ddanilets@gmail.com>
2015-12-05 04:46:34 +0200

    Signed-off-by: ddanilets <ddanilets@gmail.com>

--- react/script/stores/calendarStore.js
+++ react/script/stores/calendarStore.js
@@ -2,15 +2,16 @@
 
 var CHANGE_EVENT = 'change';
 
+var _months=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
 var _store = {
 	moment: {
-		todayYear: moment().format('YYYY'),
-		todayMonth: moment().format('MMMM'),
-		today: moment().date(),
-		moment: moment(),
-		num: moment().month() + 1,
-		name: moment().format('MMMM'),
-		year: moment().year()
+		todayYear: new Date().getFullYear(),
+		todayMonth: new Date().getMonth(),
+		today: new Date().getDate(),
+		moment: new Date().getMoment(),
+		num: new Date().getMonth() + 1,
+		name: _months[new Date().getMonth()]),
+		year: new Date().getFullYear()
 	},
 	selectedDay: {
 		year: null,
@@ -44,29 +45,20 @@ var updateMonth = function(update) {
 	if(newMonth == 0) {
 		_store.moment.year -= 1;
 		_store.moment.num = 12;
-		_store.moment.moment = moment({
-			y: _store.moment.year,
-			M: _store.moment.num - 1
-		});
-		_store.moment.name = moment(_store.moment.moment).format('MMMM');
+		_store.moment.moment = new Date(_store.moment.year,_store.moment.num - 1);
+		_store.moment.name = months[_store.moment.num - 1];
 
 	}
 	else if(newMonth == 13) {
 		_store.moment.year += 1;
 		_store.moment.num = 1;
-		_store.moment.moment = moment({
-			y: _store.moment.year,
-			M: _store.moment.num - 1
-		});
-		_store.moment.name = moment(_store.moment.moment).format('MMMM');
+		_store.moment.moment = new Date(_store.moment.year,_store.moment.num - 1);
+		_store.moment.name = months[_store.moment.num - 1];
 	}
 	else {
 		_store.moment.num += update;
-		_store.moment.moment = moment({
-			y: _store.moment.year,
-			M: _store.moment.num - 1
-		});
-		_store.moment.name = moment(_store.moment.moment).format('MMMM');
+		_store.moment.moment = new Date(_store.moment.year,_store.moment.num - 1);
+		_store.moment.name = months[newMonth];
 	}
 
 };
