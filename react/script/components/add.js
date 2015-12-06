e55fd6034f58f1b90e80c15e027ad7a2406e8387
ddanilets <ddanilets@gmail.com>
2015-12-06 09:05:34 +0200

    Signed-off-by: ddanilets <ddanilets@gmail.com>

--- react/script/components/add.js
+++ react/script/components/add.js
@@ -1,18 +1,7 @@
 /** @jsx React.DOM */
 
 var AddTask = React.createClass({
-	handleAdd: function(data) {
-		calendarActions.addTask(data);
-	},
-	getInitialState: function() {
-		return ({
-			name: "",
-			date: this.props.selectedDay.num+'.'+this.props.selectedDay.monthNum+'.'+this.props.selectedDay.year.slice(2,4),
-			holders: "",
-			description: ""
-		})
-	},
-	click: function(){
+	handleAdd: function() {
 		var self=this;
 		var date=self.state.date;
 		var day=date.slice(0,2);
@@ -29,7 +18,17 @@ var AddTask = React.createClass({
 				taskDescription: self.state.description
 			}
 		}
-		self.handleAdd.bind(null,data);
+		calendarActions.addTask(data);
+	},
+	getInitialState: function() {
+		return ({
+			name: "",
+			date: this.props.selectedDay.num+'.'+this.props.selectedDay.monthNum+'.'+this.props.selectedDay.year.slice(2,4),
+			holders: "",
+			description: ""
+		})
+	},
+	click: function(){
 	},
 	setName:function(e){
 		this.setState({name:e.target.value})
@@ -53,7 +52,7 @@ var AddTask = React.createClass({
 				<input type="text" value={self.state.date} onChange={self.setDate}  placeholder="Search here" />
 				<input type="text" value={self.state.holders} onChange={self.setHolders}  placeholder="Search here" />
 				<input type="text" value={self.state.description} onChange={self.setDescription}  placeholder="Search here" />
-				<div className="add-button" onClick={self.click}>
+				<div className="add-button" onClick={self.handleAdd.bind(null)}>
 					
 				</div>
 			</div>
