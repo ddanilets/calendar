51387601cdbd838e623326e17ba1ff89874de3f3
ddanilets <ddanilets@gmail.com>
2015-12-06 08:01:31 +0200

    Signed-off-by: ddanilets <ddanilets@gmail.com>

--- react/script/components/add.js
+++ react/script/components/add.js
@@ -53,7 +53,7 @@ var AddTask = React.createClass({
 				<input type="text" value={self.state.date} onChange={self.setDate}  placeholder="Search here" />
 				<input type="text" value={self.state.holders} onChange={self.setHolders}  placeholder="Search here" />
 				<input type="text" value={self.state.description} onChange={self.setDescription}  placeholder="Search here" />
-				<div className="add-button" onClick={self.click()}>
+				<div className="add-button" onClick={self.click}>
 					
 				</div>
 			</div>
