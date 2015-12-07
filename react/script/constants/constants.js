aaecb7a921c41963695370345a45de7a492d4f10
ddanilets <ddanilets@gmail.com>
2015-12-07 05:33:25 +0200

    Signed-off-by: ddanilets <ddanilets@gmail.com>

--- react/script/constants/constants.js
+++ react/script/constants/constants.js
@@ -7,4 +7,16 @@ var appConstants = {
 	DELETE_TASK: "DELETE_TASK"
 };
 var _months=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-var _monthsRelative=["Январь"=>"Января","Февраль"=>"Февраля","Март"=>"Марта","Апрель"=>"Апреля","Май"=>"Мая","Июнь"=>"Июня","Июль"=>"Июля","Август"=>"Августа","Сентябрь"=>"Сентября","Октябрь"=>"Октября","Ноябрь"=>"Ноября","Декабрь"=>"Декабря"];
+var _monthsRelative={};
+_monthsRelative[_months[0]]="Января";
+_monthsRelative[_months[1]]="Февраля";
+_monthsRelative[_months[2]]="Марта";
+_monthsRelative[_months[3]]="Апреля";
+_monthsRelative[_months[4]]="Мая";
+_monthsRelative[_months[5]]="Июня";
+_monthsRelative[_months[6]]="Июля";
+_monthsRelative[_months[7]]="Августа";
+_monthsRelative[_months[8]]="Сентября";
+_monthsRelative[_months[9]]="Октября";
+_monthsRelative[_months[10]]="Ноября";
+_monthsRelative[_months[11]]="Декабря";
